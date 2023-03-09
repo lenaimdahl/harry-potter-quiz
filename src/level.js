8b047e60616f52173700103f83eff2d6f2e1b6c0
Lena Imdahl <mail@lenaimdahl.de>
2023-03-09 16:16:50 +0100

    added keys to p5.js

--- src/level.js
+++ src/level.js
@@ -1,12 +1,16 @@
-// snitch p5js
+// obj p5js
 
 let speedX = 2.5;
 let speedY = 2.6;
 
 let snitch = { x: 0, y: 0 };
+let key = { x: 0, y: 0 };
 
 function preload() {
   imgSnitch = loadImage("images/snitch.png");
+  key1 = loadImage("images/key1.png");
+  key2 = loadImage("images/key2.png");
+  key3 = loadImage("images/key3.png");
 }
 function setup() {
   let canvas = createCanvas(windowWidth, 400);
@@ -14,6 +18,15 @@ function setup() {
 
   snitch.x = 0;
   snitch.y = 0;
+
+  key1.x = 3;
+  key1.y = 0;
+
+  key2.x = 4;
+  key2.y = 5;
+
+  key3.x = 10;
+  key3.y = 6;
 }
 
 function draw() {
@@ -22,10 +35,32 @@ function draw() {
 
   snitch.x = constrain(snitch.x, 10, 700);
   snitch.y = constrain(snitch.y, 10, 400);
+
+  key1.x = constrain(key1.x, 10, 700);
+  key1.y = constrain(key1.y, 10, 400);
+
+  key2.x = constrain(key2.x, 10, 700);
+  key2.y = constrain(key2.y, 10, 400);
+
+  key3.x = constrain(key3.x, 10, 700);
+  key3.y = constrain(key3.y, 10, 400);
+
   image(imgSnitch, snitch.x, snitch.y, 50, 50);
+  image(key1, key1.x, key1.y, 60, 60);
+  image(key2, key2.x, key2.y, 60, 60);
+  image(key3, key3.x, key3.y, 60, 60);
 
   snitch.x = snitch.x + random(-speedX, speedX);
   snitch.y = snitch.y + random(-speedY, speedY);
+
+  key1.x = key1.x + random(-speedX, speedX) * 3;
+  key1.y = key1.y + random(-speedY, speedY) * 2;
+
+  key2.x = key2.x + random(-speedX, speedX) * 5;
+  key2.y = key2.y + random(-speedY, speedY) * 1;
+
+  key3.x = key3.x + random(-speedX, speedX) * 3;
+  key3.y = key3.y + random(-speedY, speedY) * 4;
 }
 
 // calculating
