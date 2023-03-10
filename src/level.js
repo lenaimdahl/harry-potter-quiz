ac55239e709b117db068ab8358566fa631810db3
Lena Imdahl <mail@lenaimdahl.de>
2023-03-10 11:32:09 +0100

    fixed position of gameboard

--- src/level.js
+++ src/level.js
@@ -170,38 +170,6 @@ function generateMultiply() {
 }
 
 //hogwarts questions
-let hogwartsQA = [
-  {
-    question: "In what house is Pansy Parkinson?",
-    answers: {
-      a: "Gryffindor",
-      b: "Ravenclaw",
-      c: "Slytherin",
-      d: "Hufflepuff",
-    },
-    correctAnswer: "Slytherin",
-  },
-  {
-    question: "What is the name of Hagrid's boarhound dog?",
-    answers: { a: "Fang", b: "Bing", c: "Fluffy", d: "Snoopy" },
-    correctAnswer: "1",
-  },
-  {
-    question: "In what house is Pansy Parkinson?",
-    answers: {
-      a: "Gryffindor",
-      b: "Ravenclaw",
-      c: "Slytherin",
-      d: "Hufflepuff",
-    },
-    correctAnswer: "Slytherin",
-  },
-  {
-    question: "What is the name of Hagrid's boarhound dog?",
-    answers: { a: "Fang", b: "Bing", c: "Fluffy", d: "Snoopy" },
-    correctAnswer: "1",
-  },
-];
 
 function generateHPQuiz(index) {
   showQuestion(hogwartsQA[index].question);
